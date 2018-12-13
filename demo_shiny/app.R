054a708712f99212171c58a3f7a27aa4680a044d
JiahuiZhao <jzhao71@illinois.edu>
2018-12-13 09:58:18 -0600

    commit 1

--- demo_shiny/app.R
+++ demo_shiny/app.R
@@ -2,9 +2,10 @@ library(matrixcalc)
 library(shiny)
 library(coda)
 library(stringr)
+library(ggplot2)
 
 ui = fluidPage(
-  titlePanel("MCMC Financial Variance Reduction"),
+  titlePanel("Variance Reduction for Estimating VAR with MCMC"),
   
   sidebarLayout(
     sidebarPanel(
@@ -21,9 +22,10 @@ ui = fluidPage(
     ),
     mainPanel(
       tabsetPanel(
-        tabPanel("Delta_S", plotOutput("plot")),
+        tabPanel("Risk Factor", plotOutput("plot")),
+        tabPanel("Risk Factor Scatterplot", plotOutput("scatterplot")),
         tabPanel("Probability & Variance Factor Table", tableOutput("table"), h4("It may take a while to show up the table."))
-      )
+        )
     )))
 
 server = function(input, output) {
@@ -53,20 +55,20 @@ server = function(input, output) {
     x_var <- VAR(a0, a, A, Sigma, Delta_S, percentile)
 
    variance_general <- General(a0, a, A, Sigma, x_var)
-   variance_general <-  c(variance_general[1], variance_general[2]/variance_general[2])
+   variance_general1 <-  c(round(variance_general[1], 4), round(variance_general[2]/variance_general[2],0))
    
    variance_is <- IS(a0, a, A, Sigma, x_var)
-   variance_is <-  c(variance_is[1], variance_general[2]/variance_is[2])
+   variance_is <-  c(round(variance_is[1],4), round(variance_general[2]/variance_is[2],4))
    
    variance_cv <- CV(a0, a, A, Sigma, x_var)
-   variance_cv <-  c(variance_cv[1], variance_general[2]/variance_cv[2])
+   variance_cv <-  c(round(variance_cv[1],4), round(variance_general[2]/variance_cv[2],4))
    
    variance_ss <- SS(a0, a, A, Sigma, x_var)
-   variance_ss <-  c(variance_ss[1], variance_general[2]/variance_ss[2])
+   variance_ss <-  c(round(variance_ss[1],4), round(variance_general[2]/variance_ss[2],4))
    
-   variance <-  rbind(variance_general, variance_is, variance_cv, variance_ss)
-   colnames(variance) <-  c("Probability", "Variance")
-   variance.table <-  cbind(c("General", "Importance Sampling","Control Variates","Stratified Sampling"), variance)
+   variance <-  rbind(variance_general1, variance_cv, variance_is, variance_ss)
+   colnames(variance) <-  c("Probability", "Variance Factor")
+   variance.table <-  cbind(c("General" ,"Control Variates", "Importance Sampling","Stratified Sampling"), variance)
    
    return(variance.table)
   })
@@ -89,15 +91,37 @@ server = function(input, output) {
     
     Sigma <- cov(data.frame(delta_Bond,delta_Interest))
     Sigma <- as.matrix(Sigma)
-    a0 <- 0
-    a <- matrix(c(1,1), ncol = 1)
-    A <- matrix(c(1,0,0,1), ncol = 2, nrow = 2, byrow = TRUE)
-    percentile <- 0.9
     Delta_S <- Multi_Normal(matrix(c(0,0),ncol=1), Sigma)
     gelman.plot(mcmc.list(as.mcmc(Delta_S[,1]), as.mcmc(Delta_S[,2])))
   })
+  
+  active_scatterplot = eventReactive(input$run, {
+    A = matrix(as.numeric(str_split(input$capitalA, pattern = ",")[[1]]), 2, 2, byrow = T)
+    a = matrix(as.numeric(str_split(input$capitala, pattern = ",")[[1]]), 2)
+    a0 = as.numeric(input$capitala0)
+    
+    Interest <- read.csv("Interest.csv", header = TRUE)
+    Bond <- read.csv("Bond.csv", header = TRUE)
+    Bond <- Bond[c(625:688),2]
+    Interest <- Interest[c(745:808),2]
+    delta_Bond <- numeric(length(Bond))
+    delta_Interest <- numeric(length(Interest))
+    for(i in 2 : length(Bond)){
+      delta_Bond[i-1] <- Bond[i] - Bond[i-1]
+      delta_Interest[i-1] <- Interest[i] - Interest[i-1]
+    }
+    
+    Sigma <- cov(data.frame(delta_Bond,delta_Interest))
+    Sigma <- as.matrix(Sigma)
+    Delta_S <- Multi_Normal(matrix(c(0,0),ncol=1), Sigma)
+    x = data.frame(Delta_S)
+    ggplot(x, aes(x = x[, 1], y= x[, 2]), xlab = "a") + geom_point() + xlab("First Risk Factor") + ylab("Second Risk Factor") + ggtitle("Risk Factors") +
+      theme(plot.title = element_text(size = 20, face = "bold", hjust = 0.5))
+  })
+  
   output$plot = renderPlot({active_plot()})
-  output$table = renderTable({active_variance()}, digits = 10)
+  output$scatterplot = renderPlot({active_scatterplot()})
+  output$table = renderTable({active_variance()})
   }
   
 
